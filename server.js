8869f21c3aebff462be3610eaccb45ef185f7ff9
Luke Michalakis <michalakisluke@gmail.com>
2021-10-28 13:58:56 -0400

    <First commit>

---
+++ server.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const team = [];
+
+function init() {
+    // Prompt for team manager info
+    inquirer.prompt([
+        
+    ]).then((answers) => {
+    });
+}
+
+init();
