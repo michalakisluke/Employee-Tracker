51a05fcce2a44925cb2638773eccdfe717c83e6d
Luke Michalakis <michalakisluke@gmail.com>
2021-10-28 15:49:35 -0400

    <Build foreign keys into roles table>

--- db/schema.sql
+++ db/schema.sql
@@ -6,17 +6,15 @@ CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    -- need to make these below into foreign keys --
-    role_id INT,
-    manager_id INT,
+    CONSTRAINT fk_roleid FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+    CONSTRAINT fk_managerid FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
 
 CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(8,2) NOT NULL,
-  -- Need to make this a foreign key --
-  department_id INT 
+  CONSTRAINT fk_depid FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL 
 );
 
 CREATE TABLE departments (
