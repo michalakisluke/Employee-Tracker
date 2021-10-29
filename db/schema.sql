66d5520d85989495261391ad5350c20f6e1bbf60
Luke Michalakis <michalakisluke@gmail.com>
2021-10-28 22:19:25 -0400

    <Fix schema and seed files>

--- db/schema.sql
+++ db/schema.sql
@@ -2,23 +2,28 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS departments;
 
-CREATE TABLE employees (
+CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    CONSTRAINT fk_roleid FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
-    CONSTRAINT fk_managerid FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(8,2) NOT NULL,
+    department_id INT,
     CONSTRAINT fk_depid FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL 
 );
 
-CREATE TABLE departments (
+CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    CONSTRAINT fk_roleid FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_managerid FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
+
+
 
